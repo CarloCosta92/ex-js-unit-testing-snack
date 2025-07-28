e01f59f0954f744b533cf04b23151862bc5f244a
Carlo Costa <carlo.costa92@gmail.com>
2025-07-28 22:01:01 +0200

    feat: solved snack 1

---
+++ snack.js
@@ -0,0 +1,20 @@
+function getInitials(fullName) {
+    return fullName
+        .split(" ")
+        .map(word => word[0].toUpperCase())
+        .join("");
+}
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    getInitials
+};
--- snack.test.js
+++ snack.test.js
@@ -1,8 +1,9 @@
 // 🏆 Snack 1
 // Creare un test che verifichi la seguente descrizione:
 // 👉 "La funzione getInitials restituisce le iniziali di un nome completo."
+const { getInitials } = require("./snack.js");
 
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
-    let name = "Carlo Costa"
-    expect(name).toBe("CC")
-})
+    const result = getInitials("Carlo Costa");
+    expect(result).toBe("CC");
+});
