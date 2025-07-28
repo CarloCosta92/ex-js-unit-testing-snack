3e3c1af6defe8c34413e8fe6707dd1e149ce9044
Carlo Costa <carlo.costa92@gmail.com>
2025-07-28 21:55:23 +0200

    feat:create new project

---
+++ snack.test.js
@@ -0,0 +1,8 @@
+// 🏆 Snack 1
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione getInitials restituisce le iniziali di un nome completo."
+
+test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
+    let name = "Carlo Costa"
+    expect(name).toBe("CC")
+})
